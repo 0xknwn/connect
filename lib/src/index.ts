0d7055683e090f59f57d55a7fea482e4e15963e8
0xknwn <145777008+0xknwn@users.noreply.github.com>
2024-05-15 11:04:51 +0200

    exchange message with iframe

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,2 +1,36 @@
-import {} from "./inject";
 export {};
+
+import { injectIFrame } from "./postmate";
+
+declare global {
+  interface Window {
+    starknet_smartr: StarknetWindowObject | undefined;
+  }
+}
+
+class StarknetWindowObject {
+  getContractAddress: () => Promise<string> = async () => {
+    return "0x1234567890";
+  };
+
+  injectSmartr: (elementId: string) => Promise<void> = async (
+    elementId: string
+  ) => {
+    injectIFrame(elementId);
+    console.log("injecting smartr on", elementId);
+  };
+}
+
+export const injectSmartr = async () => {
+  console.log("injecting smartr");
+  if (window.starknet_smartr) {
+    return window.starknet_smartr;
+  }
+
+  const proxy = new StarknetWindowObject();
+  window.starknet_smartr = proxy;
+
+  return proxy;
+};
+
+injectSmartr();
