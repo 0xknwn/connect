2ffaae1a4870d046e4a01505b91becc7477f9a84
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-01 22:06:40 +0100

    create types with lib (#6)

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig(({ mode }) => ({
   build: {
@@ -16,5 +17,9 @@ export default defineConfig(({ mode }) => ({
   define: {
     "process.env.NODE_ENV": JSON.stringify(mode),
   },
-  plugins: [],
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
 }));
