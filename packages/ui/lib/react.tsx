3de4a5df21e78777b1ac4a6218a322c4d14aaad3
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-08 08:48:03 +0100

    use initial value and callback in ui from dapp (#15)

--- packages/ui/lib/react.tsx
+++ packages/ui/lib/react.tsx
@@ -1,5 +1,22 @@
-const InjectedReact = () => {
-  return <div>injected react</div>;
+import { useState, useEffect } from "react";
+
+type InjectedReactProps = {
+  initialValue?: number;
+  updateValue?: (value: number) => void;
+};
+const InjectedReact = ({ initialValue, updateValue }: InjectedReactProps) => {
+  const [value, setValue] = useState(initialValue || 0);
+  useEffect(() => {
+    if (!updateValue) return;
+    updateValue(value);
+  }, [value]);
+
+  return (
+    <>
+      <div>Value: {value}</div>
+      <button onClick={() => setValue(value + 1)}>Click me</button>
+    </>
+  );
 };
 
 export default InjectedReact;
