1053922732ab36f2eb8a904cfac3c6b697ecd1d0
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-09 15:49:45 +0100

    fix content

--- packages/app/_api/rpc.ts
+++ packages/app/_api/rpc.ts
@@ -5,14 +5,14 @@ export const edge = true;
 
 export default async function handler(request: Request) {
   const url = process.env.API_BASE_URL as string;
-  const requestBody = await request.json();
+  const requestBody = JSON.stringify(await request.json());
   console.log(requestBody);
   const output = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(request.body),
+    body: requestBody,
   });
   const responseBody = await output.json();
   console.log(responseBody);
