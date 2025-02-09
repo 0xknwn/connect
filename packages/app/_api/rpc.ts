dda022c8523a5b4c60e668ad6cb5fd81284a8f66
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-09 15:30:40 +0100

    manage content

--- packages/app/_api/rpc.ts
+++ packages/app/_api/rpc.ts
@@ -5,7 +5,8 @@ export const edge = true;
 
 export default async function handler(request: Request) {
   const url = process.env.API_BASE_URL as string;
-  console.log(request.body);
+  const requestBody = await request.json();
+  console.log(requestBody);
   const output = await fetch(url, {
     method: "POST",
     headers: {
@@ -13,10 +14,12 @@ export default async function handler(request: Request) {
     },
     body: JSON.stringify(request.body),
   });
-  const body = await output.json();
-  console.log(body);
-  if (body) {
-    return new Response(JSON.stringify(body), { status: output.status });
+  const responseBody = await output.json();
+  console.log(responseBody);
+  if (responseBody) {
+    return new Response(JSON.stringify(responseBody), {
+      status: output.status,
+    });
   }
   return new Response(`{"statusText": "Not Found"}`, { status: output.status });
 }
