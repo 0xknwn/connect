0ccad5e62b7ee9a0cd81cf4ffae9cc88d3b3666f
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-09 19:44:47 +0100

    improve deps for api

--- packages/app/_api/version.ts
+++ packages/app/_api/version.ts
@@ -1,13 +1,6 @@
-// file: _api/version.ts
-import { geolocation } from "@vercel/edge";
-
 // Should run on edge runtime
 export const edge = true;
 
 export default function handler(req: Request) {
-  let { city } = geolocation(req);
-  if (!city) {
-    city = "San Francisco";
-  }
-  return new Response(`{"version": "dev", "city": "${city}"}`);
+  return new Response(`{"version": "dev"}`);
 }
