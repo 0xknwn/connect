a5e8b6be4d074ec8207b75ca82de5ebb2e989fbe
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-20 21:53:10 +0100

    continue implementation

--- packages/lib/src/0.7/methods.ts
+++ packages/lib/src/0.7/methods.ts
@@ -1,12 +1,28 @@
-import { WALLET_API } from "@starknet-io/types-js";
+import {
+  WALLET_API,
+  ChainId,
+  Permission,
+  SpecVersion,
+} from "@starknet-io/types-js";
 
 const notImplemented = async () => {
   throw new Error("Not implemented");
 };
 
-export const requestChainId = async () => {
-  console.log("requestChainId");
+const emptyImplementation = async () => {
+  throw new Error("Should be replaced by the wallet");
+};
+export const requestChainId = async (): Promise<ChainId> => {
   return "0x534e5f5345504f4c4941";
+};
+
+// @todo: check what are the permissions when the wallet is not connected
+export const getPermissions = async (): Promise<Permission[]> => {
+  return [Permission.ACCOUNTS];
+};
+
+export const supportedSpecs = async (): Promise<SpecVersion[]> => {
+  return ["0.7"];
 };
 
 export const handlers = {
@@ -14,12 +30,12 @@ export const handlers = {
   wallet_switchStarknetChain: notImplemented,
   wallet_watchAsset: notImplemented,
   wallet_requestAccounts: notImplemented,
-  wallet_getPermissions: notImplemented,
+  wallet_getPermissions: getPermissions,
   wallet_requestChainId: requestChainId,
   wallet_deploymentData: notImplemented,
   wallet_addDeclareTransaction: notImplemented,
   wallet_addInvokeTransaction: notImplemented,
   wallet_signTypedData: notImplemented,
-  wallet_supportedSpecs: notImplemented,
-  wallet_supportedWalletApi: notImplemented,
+  wallet_supportedSpecs: supportedSpecs,
+  wallet_supportedWalletApi: emptyImplementation,
 } as Record<WALLET_API.RpcMessage["type"], (params?: any) => Promise<any>>;
--- packages/lib/src/methods.ts
+++ packages/lib/src/methods.ts
@@ -1,8 +1,17 @@
-import { WALLET_API } from "@starknet-io/types-js";
+import {
+  WALLET_API,
+  API_VERSION_NOT_SUPPORTED as API_VERSION_NOT_SUPPORTED_ERROR,
+} from "@starknet-io/types-js";
 import { handlers as handlers07 } from "./0.7/methods";
 
 const version07 = "0.7" as string;
 const defaultVersion = version07 as string;
+
+const API_VERSION_NOT_SUPPORTED: API_VERSION_NOT_SUPPORTED_ERROR = {
+  code: 162,
+  message: "An error occurred (API_VERSION_NOT_SUPPORTED)",
+  data: "string",
+};
 
 export const supportedWalletApi = async () => {
   return [version07];
@@ -19,9 +28,8 @@ export const handleMessage = <T extends WALLET_API.RpcMessage["type"]>(
   call: WALLET_API.RequestFnCall<T>
 ) => {
   const version = call.params?.api_version ?? defaultVersion;
-  console.log("version", version);
   if (!versionedHandlers[version]) {
-    throw new Error(`Unsupported API version: ${version}`);
+    throw API_VERSION_NOT_SUPPORTED;
   }
   const handlers = versionedHandlers[version];
   console.log("handlers", handlers);
